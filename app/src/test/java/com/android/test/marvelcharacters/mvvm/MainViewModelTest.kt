e81ec207b14b96148b7bf78dfbf15cae8f0c4c7c
BhoomikaBrahmbhatt <40670499+BhoomikaBrahmbhatt@users.noreply.github.com>
2022-03-08 17:02:37 +0000

    Kotlin scope added

---
+++ app/src/test/java/com/android/test/marvelcharacters/mvvm/MainViewModelTest.kt
@@ -0,0 +1,39 @@
+package com.android.test.marvelcharacters.mvvm
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class MainViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getChaptersList() {
+    }
+
+    @Test
+    fun getErrorMessage() {
+    }
+
+    @Test
+    fun getCharacterUser() {
+    }
+
+    @Test
+    fun getAllCharacters() {
+    }
+
+    fun testGetChaptersList() {}
+    fun testGetErrorMessage() {}
+    fun testGetCharacterUser() {}
+    fun testGetAllCharacters() {}
+}
